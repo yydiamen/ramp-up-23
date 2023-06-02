314b85cce3d2520fa674418ea26dc2bad801d1da
yydiamen <yydiamen@mail.yu.edu>
2023-06-02 18:42:06 -0400

    finish test

--- challenges/challanges_test.py
+++ challenges/challanges_test.py
@@ -16,5 +16,19 @@ class Challanges_Unit_Tests(unittest.TestCase):
         self.assertEqual(CodingInterview_hard.interview([5, 5, 10, 10, 15, 15, 20, 20], 120), "qualified")
     
     def test_sum(self):
-       self.assertEqual(sum_very_easy.addition(3, 4), 7) 
-        
+       self.assertEqual(sum_very_easy.addition(3, 4), 7)
+
+    def test_triangle(self):
+        self.assertEqual(triangle_very_easy.tri_area(4,4), 8)
+
+    def test_degrees(self):
+        self.assertEqual(degrees_easy.radians_to_degrees(20), 1146)
+
+    def test_length(self):
+        self.assertEqual(lengthofNumber_medium.number_length(123), 3)
+
+    def test_hill(self):
+        self.assertEqual(UpDpwnHill_hard.ave_spd(18, 20, 60), 30)
+
+    def test_Dac(self):
+        self.assertEqual(ViryualDAC_medium.V_DAC(1023), 5)
