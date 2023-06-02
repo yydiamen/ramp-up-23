d889a401a4b457fe3886a38ab8374256a200ed40
yydiamen <yydiamen@mail.yu.edu>
2023-06-01 21:26:38 -0400

    add

--- book-library/BookLibraryAPI.py
+++ book-library/BookLibraryAPI.py
@@ -1,1 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+import schemas
+import uuid
+
+app = FastAPI()
+
+BookLibrary = {
+    
+}
+
+@app.get("/books/")
+def getBooks():
+    return BookLibrary
+    
+
+@app.get("/books/{id}")
+def getBooksByID(id:str):
+    return BookLibrary[id]
+    
+@app.post("/books/")
+def addBook(book:schemas.Book):
+    newBook = len(BookLibrary.keys()) + 1
+    newid = uuid.uuid1()
+    BookLibrary[newBook] = {newid:book}
+    return newid
+    
+    
+@app.put("/books/{id}")
+def bookUpdate(id:int, book:schemas.Book):
+    BookLibrary[id]["title"] = book.title
+    BookLibrary[id]["author"] = book.author
+    BookLibrary[id]["year"] = book.year
+
+@app.delete()
