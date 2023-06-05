6ad16b6956315e677d7730c2b65b7ff03dba1293
yydiamen <yydiamen@mail.yu.edu>
2023-06-05 10:22:50 -0400

    docker

--- book-library/BookLibraryAPI.py
+++ book-library/BookLibraryAPI.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, Response
+from fastapi import FastAPI
 from pydantic import BaseModel
 from uuid import UUID,uuid4
 
@@ -16,7 +16,7 @@ db: dict[UUID, Book] = dict()
 
 app = FastAPI()
 
-@app.post("/books", status_code=status.HTTP_201_CREATED)
+@app.post("/books")
 def post_book(book_info: BookInfo):
     book: Book = Book(
         author=book_info.author,
@@ -32,28 +32,24 @@ def get_all_books():
     return [book for book in db.values()]
 
 @app.get("/books/{uid}")
-def get_book_by_id(uid: UUID, response: Response):
+def get_book_by_id(uid: UUID):
     book: Book = db.get(uid)
-    if book is None:
-        response.status_code = status.HTTP_404_NOT_FOUND
     return book
 
 @app.put("/books/{uid}")
-def update_book_by_id(uid: UUID, book_info: BookInfo, response: Response):
+def update_book_by_id(uid: UUID, book_info: BookInfo):
     book: Book = db.get(uid)
     if not book is None:
         book.author = book_info.author
         book.year = book_info.year
         book.title = book_info.title
-    else:
-        response.status_code = status.HTTP_404_NOT_FOUND
     return book
 
 @app.delete("/books/{uid}")
-def delete_book_by_id(uid: UUID, response: Response):
+def delete_book_by_id(uid: UUID):
     book = db.get(uid)
     if not book is None:
         del db[uid]
         return book
     else:
-        response.status_code = status.HTTP_404_NOT_FOUND
+        return book
